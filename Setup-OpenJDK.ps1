6f01d6fdac62c84e2556894db500917ed95bf0e7
Aren Cambre <aren@arencambre.com>
2022-06-09 10:08:34 -0500

    Update to use current OpenJDK JRE release.

--- Setup-OpenJDK.ps1
+++ Setup-OpenJDK.ps1
@@ -1,10 +1,11 @@
 $ErrorActionPreference = "Stop"
 
 $targetFolder = "e:\SolrCloud"
+$javaRelease = "https://github.com/ojdkbuild/ojdkbuild/releases/download/java-1.8.0-openjdk-1.8.0.332-1.b09/java-1.8.0-openjdk-jre-1.8.0.332-1.b09.ojdkbuild.windows.x86_64.zip"
 
 Install-Module "7Zip4Powershell"
 Import-Module ".\SolrCloud-Helpers" -DisableNameChecking
 
-Install-OpenJDK -targetFolder $targetFolder
+Install-OpenJDK -targetFolder $targetFolder -javaRelease $javaRelease
 
 Write-Host "You should refresh other PowerShell/CMD windows now..."
